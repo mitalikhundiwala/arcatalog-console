91c5597160e95a54cdee684846d7e25bf1728b8f
Mitali Patel <mitali.khundiwala@gmail.com>
2020-05-18 19:32:45 +0400

    Update Book, validation & book listing

--- src/components/books/Update.tsx
+++ src/components/books/Update.tsx
@@ -1,16 +1,12 @@
 import { Formik, Field } from 'formik';
-import {
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-    Button,
-    CircularProgress
-} from '@chakra-ui/core';
+import { Heading, CircularProgress } from '@chakra-ui/core';
 import gql from 'graphql-tag';
 import Router from 'next/router';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 import { useToast } from '@chakra-ui/core';
+import CreatableSelect from 'react-select/creatable';
+import * as Yup from 'yup';
+import BookEditor, { BookSchema } from './BookEditor';
 
 const UPDATE_BOOK_MUTATION = gql`
     mutation updateBook($id: String!, $title: String, $author: [String!]) {
@@ -79,71 +75,32 @@ const UpdateBook = ({ bookId }) => {
     if (!bookData && loading)
         return <CircularProgress isIndeterminate></CircularProgress>;
     return (
-        <Formik
-            initialValues={{
-                title: bookData?.title,
-                author: bookData.author.map(({ name }) => name).join(',')
-            }}
-            onSubmit={async (values, actions) => {
-                await updateBook({
-                    variables: {
-                        id: bookId,
-                        title: values.title,
-                        author: values.author.split(',')
-                    }
-                });
-
-                actions.resetForm();
-            }}
-        >
-            {(props) => (
-                <form onSubmit={props.handleSubmit}>
-                    <Field name="title">
-                        {({ field, form }) => (
-                            <FormControl
-                                isInvalid={
-                                    form.errors.title && form.touched.title
-                                }
-                            >
-                                <FormLabel htmlFor="title">Title</FormLabel>
-                                <Input
-                                    {...field}
-                                    id="title"
-                                    placeholder="Title"
-                                />
-                                <FormErrorMessage>
-                                    {form.errors.title}
-                                </FormErrorMessage>
-                            </FormControl>
-                        )}
-                    </Field>
-                    <Field name="author">
-                        {({ field, form }) => (
-                            <FormControl
-                                isInvalid={
-                                    form.errors.author && form.touched.author
-                                }
-                            >
-                                <FormLabel htmlFor="author">
-                                    Author Name
-                                </FormLabel>
-                                <Input
-                                    {...field}
-                                    id="author"
-                                    placeholder="Author"
-                                />
-                                <FormErrorMessage>
-                                    {form.errors.author}
-                                </FormErrorMessage>
-                            </FormControl>
-                        )}
-                    </Field>
-                    <Button mt={4} isLoading={updateBookLoading} type="submit">
-                        Submit
-                    </Button>
-                </form>
-            )}
-        </Formik>
+        <>
+            <Heading mb="8">Update Book</Heading>
+            <Formik
+                initialValues={{
+                    title: bookData?.title,
+                    author: bookData?.author.map((author) => {
+                        return {
+                            value: author.id,
+                            label: author.name
+                        };
+                    })
+                }}
+                validationSchema={BookSchema}
+                onSubmit={async (values, actions) => {
+                    await updateBook({
+                        variables: {
+                            id: bookId,
+                            title: values.title.trim(),
+                            author: values.author?.map((author) => author.label)
+                        }
+                    });
+                }}
+            >
+                {(props) => <BookEditor {...props} />}
+            </Formik>
+        </>
     );
 };
 
